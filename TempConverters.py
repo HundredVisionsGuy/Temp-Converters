b1a409527cb144d32e41f7358e15643136518e39
hundredvisionsguy <winikkc@hsd.k12.or.us>
2018-04-02 18:02:28 -0700

    adding initial files

---
+++ TempConverters.py
@@ -0,0 +1,34 @@
+# TempConverters.py
+# By ____
+# You will write 2 functions: fahrenheitToCelsius() and celsiusToFahrenheit()
+# You will want to test the two functions by running this document
+# WARNING: you must NOT change the function names, or you will fail the tests
+# NOTE: you are welcome to choose the variable names (they will not affect
+#       the tests).
+
+# TODO: write a function titled, fahrenheitToCelsius() that...
+#       receives a floating point number (fahrenheit)
+#       returns a floating point number (celsius)
+
+
+# TODO: write a function titled, celsiusToFahrenheit() that...
+#       receives a floating point number (celsius)
+#       returns a floating point number (fahrenheit)
+
+
+##################################
+## DO NOT CHANGE THE CODE BELOW ##
+##################################
+
+if __name__ == '__main__':
+    # Test fahrenheitToCelsius()
+    celsius = fahrenheitToCelsius(212)
+    print "212 degrees Fahrenheit is " + str(celsius) + " degrees celsius."
+    # output should be 100.0
+
+    # Do the same as above for the following units:
+    # 32F, 5F, 0C, 100C, -50C
+    # Expected units should be:
+    # 0.0C, -15.0C, 32.0F, 212.0F, -58.0C (respectively)
+
+    
---
+++ test_TempConverters.py
@@ -0,0 +1,56 @@
+# test_TempConverters.py
+# Tests Temp Converter functions
+
+import unittest
+import TempConverters
+
+class KnownValues(unittest.TestCase):
+    # Formula for unittest method is:
+    # test_functionName_testDescription
+    def test_fahrenheitToCelsius_for212F(self):
+        # Capture the results of the function
+        result = TempConverters.fahrenheitToCelsius(212)
+        # Check for expected output
+        self.assertEqual(100.0, result)
+
+    def test_fahrenheitToCelsius_for32F(self):
+        # Capture the results of the function
+        result = TempConverters.fahrenheitToCelsius(212)
+        # Check for expected output
+        self.assertEqual(0.0, result)
+
+    def test_fahrenheitToCelsius_for5F(self):
+        # Capture the results of the function
+        result = TempConverters.fahrenheitToCelsius(5)
+        # Check for expected output
+        self.assertEqual(-15.0, result)
+    
+    def test_fahrenheitToCelsius_for50F(self):
+        # Capture the results of the function
+        result = TempConverters.fahrenheitToCelsius(50)
+        # Check for expected output
+        self.assertEqual(40.0, result)
+
+    def test_celsiusToFahrenheit_for0C(self):
+        # Capture the results of the function
+        result = TempConverters.celsiusToFahrenheit(0)
+        # Check for expected output
+        self.assertEqual(32.0, result)
+
+    def test_celsiusToFahrenheit_for100C(self):
+        # Capture the results of the function
+        result = TempConverters.celsiusToFahrenheit(100)
+        # Check for expected output
+        self.assertEqual(212.0, result)
+
+    def test_celsiusToFahrenheit_for55C(self):
+        # Capture the results of the function
+        result = TempConverters.celsiusToFahrenheit(55)
+        # Check for expected output
+        self.assertEqual(131.0, result)
+
+    def test_celsiusToFahrenheit_forMinus50C(self):
+        # Capture the results of the function
+        result = TempConverters.celsiusToFahrenheit(-50)
+        # Check for expected output
+        self.assertEqual(-58.0, result)
